2baf86c9597a422ff16f44d8186304ffa719ba0d
raushan725707 <121337112+raushan725707@users.noreply.github.com>
2023-11-09 11:04:40 +0530

    second time commit

---
+++ typingspeed/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import SpeedTypingGame from './component/SpeedTypingGame';
+function App() {
+  return (
+    <div className="App">
+      <SpeedTypingGame />
+    </div>
+  );
+}
+
+export default App;
